c12ca8171aa0b71fa5b22a877074f5d0b2418dd9
Pythonlover-1 <83160514+Pythonlover-1@users.noreply.github.com>
2022-12-31 01:05:55 +0300

    Update __main__.py

--- programs_samples/assembler2dat/__main__.py
+++ programs_samples/assembler2dat/__main__.py
@@ -15,19 +15,23 @@ def main():
         with open(args.input_file, "r") as input_file:
             commands = read_commands(input_file)
     except FileNotFoundError:
-        logging.critical(f"File {args.input_file} not found. Please add it to currant directory!")
+        logging.critical(f"File {args.input_file} not found. Please add it to current directory!")
         exit(0)
 
     with open(args.output_file, "a" if args.append_output_file else "w") as output_file:
         fill_jump_bookmarks(commands)
-
         for i in range(len(commands)):
+            if i == len(commands) - 1:
+                pass
             try:
                 should_break_line = process_command(commands[i], output_file)
                 if i < len(commands) - 1 and should_break_line:
                     output_file.write("\n")
-            except Exception:
-                logging.warning(f"Something went wrong during processing command {' '.join(commands[i])}")
+            except Exception as e:
+                logging.warning(f"Cannot parse \"{' '.join(commands[i])}\" due to: {e}")
+    lines = open(args.output_file).readlines()
+    lines[-1] = lines[-1].rstrip('\n')
+    open(args.output_file, "w").writelines(lines)
     logging.info("Commands was imported")
 
 
